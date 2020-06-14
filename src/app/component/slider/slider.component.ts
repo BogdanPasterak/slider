e4625fd6c3be0d8ae59483fc5a648605f03f6157
bogdanpasterak <bogdanpasterak@gmail.com>
2020-06-14 23:29:43 +0100

    first show

--- src/app/component/slider/slider.component.ts
+++ src/app/component/slider/slider.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
+  images: Array<string>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.images = new Array(
+      './assets/sliderimages/img1.jpg',
+      './assets/sliderimages/img2.jpg',
+      './assets/sliderimages/img3.jpg',
+      './assets/sliderimages/img4.jpg',
+      './assets/sliderimages/img5.jpg'      
+    )
   }
 
 }
